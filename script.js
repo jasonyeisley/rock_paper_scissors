b77f0df8388cdfbc6036c368cc042f7fe8dd2fb1
jasonyeisley <117985534+jasonyeisley@users.noreply.github.com>
2022-11-25 19:50:15 -0500

    Add Computer Choice
    - Use Math.random to display the computer Choice
    - Begin framework to display answer in HTML

---
+++ script.js
@@ -0,0 +1,16 @@
+function get_computer_choice () {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[getRandomInt(0, 2)]
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//console.log(get_computer_choice())
+
+function display_comp_choice() {
+    document.getElementById('comp_choice').innerHTML = get_computer_choice()
+}
